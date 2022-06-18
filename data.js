3d66fa7d238843ea671e0f2d827bd256b6bd0f60
Omarali1020 <93990428+Omarali1020@users.noreply.github.com>
2022-06-18 13:19:16 +0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,15 @@
+var number1 = 2
+var number2 = 3
+var result = number1 + number2
+console.log(result)
+var button = document.querySelector('button');
+
+/*button.onclick = function () {
+    alert("Hello, World!");
+}*/
+
+var button = document.querySelector('button'); 
+
+button.onclick =function () {
+    alert(" hello world")
+}
